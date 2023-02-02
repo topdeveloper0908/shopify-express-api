33ead8a41e35a3899f9e2129615e40868a724486
fantasyknight <angrybird02211@gmail.com>
2023-02-02 11:59:14 +0100

    all changed

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ var app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', async function (req, res) {
+app.post('/', async function (req, res) {
 
     const shopify = new Shopify({
-        shopName: 'elle-1107',
-        accessToken: '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'
+        shopName: req.body.shopName,
+        accessToken: req.body.accessToken
     });
 
 
@@ -33,14 +33,14 @@ app.get('/', async function (req, res) {
         //  });
         discounts = elements;
     });
-    console.log(discounts, products);
+
     await shopify.asset.update(id, {
         key: "assets/product.js",
-        value: "document.addEventListener('DOMContentLoaded', function(event) { document.getElementById('allData').setAttribute('discounts', '" + JSON.stringify(discounts) + "'); document.getElementById('allData').setAttribute('products', '" + JSON.stringify(products) + "'); let  cusEve = new CustomEvent('added'); document.dispatchEvent(cusEve); });"
+        value: "document.addEventListener('DOMContentLoaded', function(event) { document.getElementById('allData').setAttribute('discounts', '" + JSON.stringify(discounts) + "'); console.log('req': " + req.body + ") document.getElementById('allData').setAttribute('products', '" + JSON.stringify(products) + "'); let  cusEve = new CustomEvent('added'); document.dispatchEvent(cusEve); });"
     });
 
 
-    res.send("hello world12334132423435354");
+    res.send(products);
 });
 app.listen(3000, function () { return console.log("listening on 3000"); });
 module.exports = app;
